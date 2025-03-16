08b08a5e23286c413534366f3d919de1a5254cf2
gettygetty08 <gettygetty08@gmail.com>
2025-03-17 00:18:40 +0900

    Chaining Assertionを使った例外テストの書き方

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c0b837c-4681-4c40-9d4f-340457d552d1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3,Class1.Add(1,2));
+            
+        }
+
+        //普通に例外のテストをする場合はこんな感じ
+        [TestMethod]
+        [ExpectedException(typeof(InputException))]
+        public void 例外のテスト()
+        {
+            Assert.AreEqual(3, Class1.Add(-1, 2));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //chaining Assertionを使うとこんな書き方も
+            Class1.Add(1, 2).Is(3);
+
+            //chaining Assertionを使った例外テストの書き方
+            var ex = AssertEx.Throws<InputException>(() => Class1.Add(-1, 2));
+            Assert.AreEqual("マイナス値は入力できません。", ex.Message);
+            ex.Message.Is("マイナス値は入力できません。");
+        }
+
+    }
+}
